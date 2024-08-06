fbcbeeeb54485511d58b93192cae3c25a934aaed
tawanNaw <160324677+tawanNaw@users.noreply.github.com>
2024-08-07 03:00:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import NodeNetwork from './components/NodeNetwork';
 import Footer from './components/Footer';
 import FirstImpact from './components/FirstImpact';
+import ChatBot from './components/Chatbot';
 import './App.css'; // นำเข้าไฟล์ CSS
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
       <div className="footer">
         <Footer />
       </div>
+      <ChatBot /> {/* เพิ่ม Chatbot component */}
     </div>
   );
 };
