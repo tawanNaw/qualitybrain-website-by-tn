f3d67ceed9b89047f8b0ff0a9792cb3fabaeb0b4
tawanNaw <160324677+tawanNaw@users.noreply.github.com>
2024-08-07 02:58:51 +0700

    add chatbot function

---
+++ src/components/Chatbot.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './Chatbot.css';
+
+const ChatBot = () => {
+  const [messages, setMessages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const chatMessagesRef = useRef(null);
+
+  const sendMessage = async () => {
+    if (searchQuery) {
+      addMessage('user', searchQuery);
+      try {
+        const response = await fetch('http://127.0.0.1:5000/search-image', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ search_query: searchQuery })
+        });
+        if (response.ok) {
+          const contentType = response.headers.get('Content-Type');
+          if (contentType && contentType.includes('application/json')) {
+            const data = await response.json();
+            addMessage('bot', `<p>${data.response}</p>`);
+          } else {
+            const blob = await response.blob();
+            const url = URL.createObjectURL(blob);
+            addMessage('bot', `<img src="${url}" alt="Image" />`);
+          }
+        } else {
+          const error = await response.json();
+          addMessage('bot', `<p style="color:red;">${error.error}</p>`);
+        }
+      } catch (error) {
+        addMessage('bot', `<p style="color:red;">Error: ${error.message}</p>`);
+      }
+      setSearchQuery('');
+    }
+  };
+
+  const addMessage = (sender, message) => {
+    setMessages(prevMessages => [...prevMessages, { sender, message }]);
+  };
+
+  useEffect(() => {
+    if (chatMessagesRef.current) {
+      chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  return (
+    <div className="chatbot-container">
+      <div className="chat-header">Chatbot Web App</div>
+      <div className="chat-messages" id="chatMessages" ref={chatMessagesRef}>
+        {messages.map((msg, index) => (
+          <div key={index} className={`chat-message ${msg.sender}`}>
+            <div className="chat-message-content" dangerouslySetInnerHTML={{ __html: msg.message }}></div>
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input 
+          type="text" 
+          id="search_query" 
+          placeholder="Type your message here..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button id="sendButton" onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBot;
